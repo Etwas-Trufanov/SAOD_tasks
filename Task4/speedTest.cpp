0a2150c6ec1afa4dc73ab19702de572756c1a134
Etwas-Trufanov <trufanov.v.2005@ro.ru>
2026-05-25 01:37:29 +0900

    Добавил графики

--- Task4/speedTest.cpp
+++ Task4/speedTest.cpp
@@ -10,7 +10,7 @@ int main() {
 
     // Слошной тест скорости
     std::cout << "bubbleSort с шагом 1000, до 20000, время в мкс" << std::endl;
-    for (std::size_t size = 0; size < 20000; size += 1000) {
+    for (std::size_t size = 0; size <= 20000; size += 1000) {
         std::vector<int> array = fill_random_vector<int>(size, 0, INT_MAX);
         auto time = TTimer::measureNano([&array]() {
             array = slowSort::bubbleSort(array);
@@ -21,7 +21,7 @@ int main() {
 
     // Слошной тест скорости
     std::cout << "insertSort с шагом 1000, до 20000, время в мкс" << std::endl;
-    for (std::size_t size = 0; size < 20000; size += 1000) {
+    for (std::size_t size = 0; size <= 20000; size += 1000) {
         std::vector<int> array = fill_random_vector<int>(size, 0, INT_MAX);
         auto time = TTimer::measureNano([&array]() {
             array = slowSort::insertSort(array);
@@ -32,7 +32,7 @@ int main() {
 
     // Слошной тест скорости
     std::cout << "quickSort с шагом 1000, до 20000, время в мкс" << std::endl;
-    for (std::size_t size = 0; size < 20000; size += 1000) {
+    for (std::size_t size = 0; size <= 20000; size += 1000) {
         std::vector<int> array = fill_random_vector<int>(size, 0, INT_MAX);
         auto time = TTimer::measureNano([&array]() {
             array = fastsort::quickSort(array);
@@ -44,7 +44,7 @@ int main() {
 
     std::cout << "Отдельно смотрим quickSort на 268435456 элементов ~1 гиг\n";
     std::vector<int> array = fill_random_vector<int>(268435456, 0, INT_MAX);
-    std::cout << "Сгенерирован, спин 5 сек.\n";
+    std::cout << "Сгенерирован, спим 5 сек.\n";
     sleep(5);
     auto time = TTimer::measureNano([&array]() {
         array = fastsort::quickSort(array);
